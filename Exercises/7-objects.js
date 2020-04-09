1033fa5b1f488c8f7957536d3b31c275547bc8ad
Egor Batrakov <goshanprm@gmail.com>
2020-04-09 19:15:34 +0300

    Complete exercises 7 and 8

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const obj1 = { name: 'Egor' };
+  let obj2 = { name: 'Goga' };
+
+  obj1.name = 'Egor1';
+  obj2.name = 'Goga1';
+
+  obj2 = { name: 'Goga the First' };
+};
 
 module.exports = { fn };
